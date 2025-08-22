ddf6f382539219922a8e3c02daa71f2f4c576267
FieldOfRice <laofuke168@tutamail.com>
2025-08-22 08:43:35 +0200

    change needs more work to do it with one call

--- 12_pip_install.sh
+++ 12_pip_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker exec -it notebooks bash -c "pip install whois pandas ddgs beautifulsoup4 scikit-learn ipywidgets transformers sentence_transformers torch"
+docker exec -it notebooks bash  # PIP_DIR must be used ...
+# pip install whois pandas ddgs beautifulsoup4 scikit-learn ipywidgets transformers sentence_transformers torch
